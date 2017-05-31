39a7bd4f18b16ccc6d3f632ac3d33e68c0b7cab9
zhangzhen <zhangzhen@wochu.cn>
2017-05-31 19:24:41 +0800

    fix publish

--- ci/op_publish.sh
+++ ci/op_publish.sh
@@ -4,7 +4,8 @@
 #docker run --name hiwind_tomcat -d tomcat 
 #docker run --privileged --cap-add SYS_ADMIN -e container=docker -it --name hiwind_tomcat -p 0.0.0.0:8080:8080 -d --restart=always tomcat
 mkdir -p /home/tomcat/webapps/
+rm -f /home/tomcat/webapps/hiwind.war
 cd ..
-jar -cvf /home/zz/tomcat/webapps/hiwind.war */.
+jar -cvf /home/tomcat/webapps/hiwind.war */ .
 docker rm -f  hiwind_tomcat
 sudo docker run --name hiwind_tomcat -d -p 8080:8080 -v /home/tomcat/webapps/:/usr/local/tomcat/webapps  tomcat
